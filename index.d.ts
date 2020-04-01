b02ad186704db7e9806f2199ad6e0ece2de7f2df
Izzuddin Natsir <natsirizzuddin@gmail.com>
2020-04-01 12:49:00 +0700

    chore(build): Build for updated type definition.

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export declare type FnAddBase = (styles: object) => void;
 export declare type FnConfig = (key: string) => object;
+export declare type FnChangeRemValue = (addBase: FnAddBase, config: FnConfig) => void;
